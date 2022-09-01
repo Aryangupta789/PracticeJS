a102ecd6464a9117f25a207d43b78abd2b29e668
Aryangupta789 <iamaryangupta78@gmail.com>
2022-09-01 16:42:15 +0530

    Numbers and dates and Intl Examples and theory

---
+++ PracticeJS/jsCode/Number-Dates-Intl-Timers/script.js
@@ -0,0 +1,215 @@
+// all numbers in the javascript is represented by the decimal number format
+console.log(23===23.0) // this will return true
+
+console.log(0.1+0.2===0.3) // this is an error in js here it will show false
+
+//to convert a string in a number
+console.log(Number('22'))
+console.log(+'22') //=> this will work due to the plus sign the js will do Type Coercion 
+
+//parsing the number from a string
+console.log(Number.parseInt('36px'))
+console.log(Number.parseInt('p22x'))   //this will not give the number since the number is not in the starting of the string
+
+console.log(Number.parseInt('3.66px'))
+console.log(Number.parseFloat('3.66px'))
+
+//ANOTHER METHODS
+//  Number.isNaN()
+//  Number.isFinite()   ==>it is the best method to check a number
+
+
+//MATH
+console.log(Math.sqrt(36))
+console.log(36**(1/2))
+
+console.log(Math.max(5,18,25,44,8))
+console.log(Math.max(5,8,'55',6,3))
+console.log(Math.max(5,46,4,'55px',5)
+)
+
+console.log(Math.PI)
+console.log(Math.trunc(Math.random()*6)+1)
+
+//random number b/w min and max
+const randomNum= (min, max)=>Math.trunc(Math.random()*(max-min)+1)
+
+console.log(randomNum(5,15))
+
+//another functions
+// round
+//trunc
+// ceil
+// floor
+
+// Rounding decimals
+console.log((2.7).toFixed(0));
+console.log((2.7).toFixed(3));
+console.log((2.345).toFixed(2));
+console.log(+(2.345).toFixed(2));
+
+// The Remainder Operator
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2 * 2 + 1
+
+console.log(8 % 3);
+console.log(8 / 3); // 8 = 2 * 3 + 2
+
+console.log(6 % 2);
+console.log(6 / 2);
+
+console.log(7 % 2);
+console.log(7 / 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
+// labelBalance.addEventListener('click', function () {
+//   [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+//     // 0, 2, 4, 6
+//     if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+//     // 0, 3, 6, 9
+//     if (i % 3 === 0) row.style.backgroundColor = 'blue';
+//   });
+// });
+
+// Numeric Separators
+//use only when the numbers are stores as numbers if numbers are stored in strings then it will not work
+// 287,460,000,000
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const price = 345_99;
+console.log(price);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+const PI = 3.1415;
+console.log(PI);
+
+console.log(Number('230_000'));   //here js will not consider it as a number
+console.log(parseInt('230_000'));  // =>230
+
+
+// Working with BigInt
+console.log(2 ** 53 - 1);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(2 ** 53 + 1);
+console.log(2 ** 53 + 2);
+console.log(2 ** 53 + 3);
+console.log(2 ** 53 + 4);
+
+console.log(4838430248342043823408394839483204n);    // by putting the n we can make a number as a bigInt
+console.log(BigInt(48384302));
+
+// Operations
+console.log(10000n + 10000n);
+console.log(36286372637263726376237263726372632n * 10000000n);
+// console.log(Math.sqrt(16n));   => does not works
+
+const huge = 20289830237283728378237n;
+const num1 = 23;
+console.log(huge * BigInt(num1));
+//bigInt cant be used with the other dataTypes like huge +num
+
+// Exceptions
+console.log(20n > 15);
+console.log(20n === 20);
+console.log(typeof 20n);
+console.log(20n == '20');
+
+console.log(huge + ' is REALLY big!!!');
+
+// Divisions
+console.log(11n / 3n);
+console.log(10 / 3);
+/////////////////////////
+// Creating Dates
+
+// Create a date
+
+const now = new Date();
+console.log(now);
+
+console.log(new Date('Aug 02 2020 18:05:41'));
+console.log(new Date('December 24, 2015'));
+// console.log(new Date(account1.movementsDates[0]));
+
+console.log(new Date(2037, 10, 19, 15, 23, 5));
+console.log(new Date(2037, 10, 31));
+// in javascripts the months are zero index based
+
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+
+// Working with dates
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());  //dont use getYear
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2142256980000));
+
+console.log(Date.now());
+
+future.setFullYear(2040);
+console.log(future);
+
+///////////////////////////////////////
+// Operations With Dates
+// const future = new Date(2037, 10, 19, 15, 23);
+console.log(+future);  //=> it gives milisceounds and this can be use as a number which further can be used to do any operations  Eg:
+
+
+const calcDaysPassed = (date1, date2) =>
+  Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+
+const days1 = calcDaysPassed(new Date(2037, 3, 4), new Date(2037, 3, 14));
+console.log(days1);
+
+// Internationalizing Numbers (Intl)
+const num = 3884764.23;
+
+const options = {
+  style: 'currency',
+  unit: 'celsius',
+  currency: 'EUR',
+  // useGrouping: false,
+};
+
+console.log('US:      ', new Intl.NumberFormat('en-US', options).format(num));
+console.log('Germany: ', new Intl.NumberFormat('de-DE', options).format(num));
+console.log('Syria:   ', new Intl.NumberFormat('ar-SY', options).format(num));
+// console.log(
+//   navigator.language,
+//   new Intl.NumberFormat(navigator.language, options).format(num)
+// );
+
+
+// setTimeout
+const ingredients = ['olives', 'spinach'];
+const pizzaTimer = setTimeout(
+  (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2} 🍕`),
+  3000,
+  ...ingredients
+);
+console.log('Waiting...');
+
+if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
+
+// setInterval
+setInterval(function () {
+  const now = new Date();
+  console.log(now);
+}, 1000);
