a3555d18efaf47a734fe374439acf5d10eb4f2da
Aryangupta789 <iamaryangupta78@gmail.com>
2022-09-04 11:29:08 +0530

    sticky navigation

--- PracticeJS/Project/BankistWebPage/script.js
+++ PracticeJS/Project/BankistWebPage/script.js
@@ -128,6 +128,14 @@ const handleHover= function(e){
 nav.addEventListener('mouseover',handleHover.bind(0.5))
 nav.addEventListener('mouseout',handleHover.bind(1))
 
+//sticky Navigation
+const initialCoords= section1.getBoundingClientRect();
+window.addEventListener('scroll', function(){
+  if(window.scrollY>initialCoords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky')
+})
+
+
 
 /*
 //////////////////////////////////////
